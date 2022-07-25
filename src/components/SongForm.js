525ac3519d54dfebdc2bc371aabe43209c40112b
majaliju <majalijunyc@gmail.com>
2022-07-25 17:43:38 -0400

    updated SongForm to handle empty form entries

--- src/components/SongForm.js
+++ src/components/SongForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import {useForm} from "react-hook-form";
+import { useForm } from "react-hook-form";
 import {
   FormControl,
   FormLabel,
@@ -10,34 +10,13 @@ import {
   Flex,
   Text,
   Button,
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
 } from "@chakra-ui/react";
 
 function SongForm({ onSubmit }) {
-
-
-/*
-import React from 'react';
-import { useForm } from 'react-hook-form';
-
-export default function App() {
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = data => console.log(data);
-  console.log(errors);
-  
-  return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="Artist/Band Name" {...register("Artist/Band Name", {required: true, maxLength: 80})} />
-      <input type="text" placeholder="Song Name" {...register("Song Name", {required: true, maxLength: 100})} />
-
-      <input type="submit" />
-    </form>
-  );
-}
-*/
-
-
-
-
   // this holds the values of form as it's being updated
   const [form, setForm] = useState({
     artistName: "",
@@ -55,34 +34,45 @@ export default function App() {
   // handleSubmit for the button, sends it up to SongRequests
   const handleSubmit = (e) => {
     e.preventDefault();
-    onSubmit(form);
+    if (form.artistName !== "" || form.songName !== "") {
+      onSubmit(form);
+    } else {
+      <Alert status="error">
+        <AlertIcon />
+        <AlertTitle>Enter a real artist!</AlertTitle>
+        <AlertDescription>
+          I won't cover John Cage 4'33, sorry.
+        </AlertDescription>
+      </Alert>;
+    }
   };
 
   return (
     <>
       <Box>
         <Center>
-        <Text fontSize="xl" fontWeight="light" fontFamily="Helvetica">
-          ADD A SONG YOU'D LIKE ME TO COVER
-        </Text>
+          <Text fontSize="xl" fontWeight="light" fontFamily="Helvetica">
+            ADD A SONG YOU'D LIKE ME TO COVER
+          </Text>
         </Center>
-        
       </Box>
       <SimpleGrid>
         <Flex>
-          <FormControl >
+          <FormControl>
             <FormLabel htmlFor="artistName"></FormLabel>
             <Center>
-            <Input
-              focusBorderColor="lime"
-              id="artistName"
-              type="text"
-              variant="flushed"
-              htmlSize={55} width='auto'
-              placeholder="Artist/Band Name"
-              value={form.artistName}
-              onChange={handleChange}
-            /></Center>
+              <Input
+                focusBorderColor="lime"
+                id="artistName"
+                type="text"
+                variant="flushed"
+                htmlSize={55}
+                width="auto"
+                placeholder="Artist/Band Name"
+                value={form.artistName}
+                onChange={handleChange}
+              />
+            </Center>
           </FormControl>
         </Flex>
       </SimpleGrid>
@@ -91,28 +81,32 @@ export default function App() {
           <FormControl>
             <FormLabel htmlFor="songName"></FormLabel>
             <Center>
-            <Input
-              focusBorderColor="lime"
-              id="songName"
-              type="text"
-              variant="flushed"
-              htmlSize={55} width='auto'
-              placeholder="Song Name"
-              value={form.songName}
-              onChange={handleChange}
-            /></Center>
+              <Input
+                focusBorderColor="lime"
+                id="songName"
+                type="text"
+                variant="flushed"
+                htmlSize={55}
+                width="auto"
+                placeholder="Song Name"
+                value={form.songName}
+                onChange={handleChange}
+              />
+            </Center>
           </FormControl>
         </Box>
       </SimpleGrid>
       <Center>
-      <Button onClick={handleSubmit} 
-      borderRadius='md'
-      bgGradient='linear(to-l, #7928CA, #FF0080)'
-      _hover={{
-        bgGradient: 'linear(to-r, red.500, yellow.500)',
-      }}>
-        submit!
-      </Button>
+        <Button
+          onClick={handleSubmit}
+          borderRadius="md"
+          bgGradient="linear(to-l, #7928CA, #FF0080)"
+          _hover={{
+            bgGradient: "linear(to-r, red.500, yellow.500)",
+          }}
+        >
+          submit!
+        </Button>
       </Center>
     </>
   );
